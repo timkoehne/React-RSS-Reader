6203435a96c6c9a25f04d49660feb45940b51c75
Tim Köhne <tim.koehne97@gmail.com>
2023-05-18 11:34:07 +0200

    tree view displays feed list

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,12 +2,23 @@ import * as React from 'react'
 import './App.css'
 import RssTable from './RssTable'
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
-import TreeView from '@mui/lab/TreeView';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import TreeItem from '@mui/lab/TreeItem';
+import IconExpansionTreeView from './IconExpansionTreeView';
 
 export default function App() {
+
+  const [treeData, setTreeData] = React.useState(() => [])
+
+  React.useEffect(() => {
+    var feedList
+    fetch("http://localhost:3001/feedlist")
+      .then((res) => res.json())
+      .then((feeddata) => {
+        feedList = feeddata
+        console.log("feedList:")
+        console.log(feedList)
+        setTreeData(feedList)
+      });
+  }, [])
 
   const [rowsData, setRowsData] = React.useState(() => {
     const localValue = localStorage.getItem("ITEMS")
@@ -72,22 +83,7 @@ export default function App() {
     <>
       <PanelGroup direction="horizontal" className='panelgroup'>
         <Panel defaultSize={20} minSize={20} className='panel sidebar'>
-          <TreeView
-            aria-label="file system navigator"
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-            sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-          >
-            <TreeItem nodeId="1" label="Applications">
-              <TreeItem nodeId="2" label="Calendar" />
-            </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-              <TreeItem nodeId="10" label="OSS" />
-              <TreeItem nodeId="6" label="MUI">
-                <TreeItem nodeId="8" label="index.js" />
-              </TreeItem>
-            </TreeItem>
-          </TreeView>
+          <IconExpansionTreeView treeData={treeData}/>
         </Panel>
         <PanelResizeHandle className='panelResizeHandle' />
         <Panel minSize={30} className='panel'>
---
+++ frontend/IconExpansionTreeView.jsx
@@ -0,0 +1,152 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem, { useTreeItem } from '@mui/lab/TreeItem';
+import clsx from 'clsx';
+import Typography from '@mui/material/Typography';
+
+const CustomContent = React.forwardRef(function CustomContent(props, ref) {
+  const {
+    classes,
+    className,
+    label,
+    nodeId,
+    icon: iconProp,
+    expansionIcon,
+    displayIcon,
+  } = props;
+
+  const {
+    disabled,
+    expanded,
+    selected,
+    focused,
+    handleExpansion,
+    handleSelection,
+    preventSelection,
+  } = useTreeItem(nodeId);
+
+  const icon = iconProp || expansionIcon || displayIcon;
+
+  const handleMouseDown = (event) => {
+    //console.log("handleMouseDown")
+    preventSelection(event);
+  };
+
+  const handleExpansionClick = (event) => {
+    //console.log("handleExpansionClick")
+    handleExpansion(event);
+  };
+
+  const handleSelectionClick = (event) => {
+    //console.log("handleSelectionClick")
+    //console.log(event)
+    handleSelection(event);
+  };
+
+  return (
+    <div
+      className={clsx(className, classes.root, {
+        [classes.expanded]: expanded,
+        [classes.selected]: selected,
+        [classes.focused]: focused,
+        [classes.disabled]: disabled,
+      })}
+      onMouseDown={handleMouseDown}
+      ref={ref}
+    >
+      <div onClick={handleExpansionClick} className={classes.iconContainer}>
+        {icon}
+      </div>
+      <Typography
+        onClick={handleSelectionClick}
+        component="div"
+        className={classes.label}
+      >
+        {label}
+      </Typography>
+    </div>
+  );
+});
+
+CustomContent.propTypes = {
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object.isRequired,
+  /**
+   * className applied to the root element.
+   */
+  className: PropTypes.string,
+  /**
+   * The icon to display next to the tree node's label. Either a parent or end icon.
+   */
+  displayIcon: PropTypes.node,
+  /**
+   * The icon to display next to the tree node's label. Either an expansion or collapse icon.
+   */
+  expansionIcon: PropTypes.node,
+  /**
+   * The icon to display next to the tree node's label.
+   */
+  icon: PropTypes.node,
+  /**
+   * The tree node label.
+   */
+  label: PropTypes.node,
+  /**
+   * The id of the node.
+   */
+  nodeId: PropTypes.string.isRequired,
+};
+
+function CustomTreeItem(props) {
+  return <TreeItem ContentComponent={CustomContent} {...props} />;
+}
+
+function mapToTree(treeData) {
+  return treeData.map((data, index) => {
+    const { nodeId, label, children } = data;
+
+    return (
+      <CustomTreeItem key={index} nodeId={nodeId} label={label}>
+        {(children !== undefined && children.length > 0) && mapToTree(children)}
+      </CustomTreeItem>
+    )
+  })
+}
+
+export default function IconExpansionTreeView({ treeData }) {
+
+  function findNode(searchId, inData) {
+    //console.log("searching for nodeId " + searchId)
+    for (var i = 0; i < inData.length; i++) {
+      if (inData[i].nodeId === searchId) {
+        return inData[i]
+      } else if ((inData[i].children !== undefined && inData[i].children.length > 0)) {
+
+        var subtree = findNode(searchId, inData[i].children)
+        if (subtree){
+          return subtree
+        }
+      }
+    }
+  }
+
+  return (
+
+    <TreeView
+      aria-label="icon expansion"
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+      defaultExpanded={["0"]}
+      //onNodeToggle={}
+      onNodeSelect={(event, nodeId) => console.log(findNode(nodeId, treeData))}
+      sx={{ height: "100%", flexGrow: 1, maxWidth: 400, overflowY: 'auto', 'textAlign': 'left' }}
+    >
+      {mapToTree(treeData)}
+    </TreeView >
+  );
+}
